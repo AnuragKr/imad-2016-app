fcf2f6ed5bf1a8dd4fd1710a13e5a5855ec25b9a
AnuragKr <Anuragkrsingh5963@gmail.com>
2016-09-30 12:43:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,13 @@ console.log('Loaded!');
 //Change the text of the main-text div
 var element = document.getElementById('main-text');
 element.innerHTML = 'Welcome Bitches!' ;
+//Move the image
+var img = document.getElementById('madi');
+var marginLeft = 0;
+function moveRight() {
+    marginLeft = marginLeft + 5;
+    img.style.marginLeft = marginLeft + 'px';
+}
+img.onclick = function() {
+    var interval = setInterval(moveRight,5);
+};
