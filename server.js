43477ad72bf17e86a67d5573f5ba16c372219746
AnuragKr <Anuragkrsingh5963@gmail.com>
2016-09-30 11:36:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,16 +8,39 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-var articleOne = {
-    title: 'Article One | Anurag Kr',
-    heading: 'Article One',
+var articles = {
+  'article-one': {
+  title: 'Article One | Anurag Kr',
+  heading: 'Article One',
+  date: 'Sep 5,2016',
+  content:`
+      <p>
+            This is the content of my first article.This is the content of my first article.This is the content of my first article.
+            This is the content of my first article.This is the content of my first article.This is the content of my first article.
+     </p>
+    `
+    },
+  'article-two': {
+      title: 'Article Two | Anurag Kr',
+    heading: 'Article Two',
     date: 'Sep 5,2016',
     content:`
       <p>
             This is the content of my first article.This is the content of my first article.This is the content of my first article.
             This is the content of my first article.This is the content of my first article.This is the content of my first article.
      </p>
-    `
+     `
+  },
+  'article-three': {
+      title: 'Article Three | Anurag Kr',
+    heading: 'Article Three',
+    date: 'Sep 5,2016',
+    content:`
+      <p>
+            This is the content of my first article.This is the content of my first article.This is the content of my first article.
+     </p>
+     `
+  }
 };
 function createTemplate (data) {
  var title = data.title;
@@ -55,12 +78,11 @@ function createTemplate (data) {
   return htmlTemplate;  
 }
 
-app.get('/article-one',function(req,res){
-    res.send(createTemplate(articleOne));
-});
-
-app.get('/article-two',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+app.get('/:articleName',function(req,res){
+    //articleName = article-one
+    //articles[articleName] == {}  create object for article one 
+    var articleName = req.params.articleName;
+    res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
